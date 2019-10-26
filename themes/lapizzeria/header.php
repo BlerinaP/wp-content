2a877bcc93f05f08c77744d0881b9ce861ceff7a
BlerinaP <blerina1.p@gmail.com>
2019-10-26 18:30:53 +0200

    Creating mobile menu, and making it responsive in mobile device and desktop.

--- themes/lapizzeria/header.php
+++ themes/lapizzeria/header.php
@@ -8,37 +8,38 @@
 <body>
 
 <header class="site-header">
-    <div class="logo">
-        <a href="<?php echo esc_url(home_url('/') );?>">
-          <img src="<?php echo get_template_directory_uri();?>/img/logo.svg" class="logoimage">
-        </a>
-    </div> <!-- logo-->
-
-    <div class="header-information">
-
-        <div class="socials">
-            <?php
-              $args = array(
-               'theme_location'  => 'social-menu',
-               'container'       => 'nav',
-               'container_class' => 'socials',
-               'container_id'    => 'socials',
-               'link_before'     => '<span class="sr-text">',
-               'link_after'      => '</span'
-              );
-              wp_nav_menu($args);
-            ?>
-        </div><!--socials-->
-
-        <div class="address">
-            <p>8179 Bay Avenue Mountain View, CA 94043</p>
-            <p>Phone Number: +1-92-456-7890</p>
-        </div>
-    </div><!--header-information-->
-
+   <div class="container">
+      <div class="logo">
+             <a href="<?php echo esc_url(home_url('/') );?>">
+                <img src="<?php echo get_template_directory_uri();?>/img/logo.svg" class="logoimage">
+              </a>
+        </div> <!-- logo-->
+          <div class="header-information">
+             <div class="socials">
+               <?php
+                  $args = array(
+                  'theme_location'  => 'social-menu',
+                  'container'       => 'nav',
+                  'container_class' => 'socials',
+                  'container_id'    => 'socials',
+                  'link_before'     => '<span class="sr-text">',
+                  'link_after'      => '</span>'
+                  );
+                wp_nav_menu($args);
+             ?>
+              </div><!--socials-->
+              <div class="address">
+                <p>8179 Bay Avenue Mountain View, CA 94043</p>
+                <p>Phone Number: +1-92-456-7890</p>
+              </div>
+          </div><!--header-information-->
+   </div>
 </header>
 <div class="main-menu">
-    <div class="navigation">
+    <div class="mobile-menu">
+        <a href="#"class="mobile"><i class="fa fa-bars"></i> Menu</a>
+    </div>
+    <div class="navigation container">
         <?php
         $args=array(
           'theme_location'  => 'header-menu',
