d8c224a767fbfae40fb435d977adb47543fd342c
BlerinaP <blerina1.p@gmail.com>
2019-10-26 11:37:30 +0200

    Adding font awesome

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -2,10 +2,12 @@
 function lapizzeria_styles(){
     // adding stylesheet( register stylesheet)
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css',array(), '8.0.0');
+    wp_register_style('fontawesome', get_template_directory_uri() . '/css/fontawesome.css',array(), ' 5.11.2');
     wp_register_style('style', get_template_directory_uri() . '/style.css',array('normalize'), '1.0');
 
     //Than we have to enqueue stylesheet that has been registered
     wp_enqueue_style('normalize');
+    wp_enqueue_style('fontawesome');
     wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts','lapizzeria_styles');
