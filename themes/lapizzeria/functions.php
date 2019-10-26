d13e54c92b85a7b3909e1d9774f74cfdeb52557f
BlerinaP <blerina1.p@gmail.com>
2019-10-26 10:16:29 +0200

    Creating Menu in WP

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -1,7 +1,7 @@
 <?php
 function lapizzeria_styles(){
     // adding stylesheet( register stylesheet)
-    wp_register_style('normalize', get_template_directory_uri() . 'css\normalize.css',array(), '8.0.0');
+    wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css',array(), '8.0.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css',array('normalize'), '1.0');
 
     //Than we have to enqueue stylesheet that has been registered
@@ -9,3 +9,11 @@ function lapizzeria_styles(){
     wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts','lapizzeria_styles');
+
+//Add menus
+function lapizzeria_menus(){
+  register_nav_menus(array(
+     'header-menu' => __('Header Menu','lapizza')
+  ));
+}
+add_action('init','lapizzeria_menus');
