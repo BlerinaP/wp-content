485f8fc27d39986f0ce0c9da411fa9c68a6646b8
BlerinaP <blerina1.p@gmail.com>
2019-10-28 11:55:03 +0100

    Adding google fonts into the theme

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -2,6 +2,7 @@
 function lapizzeria_styles(){
     // adding stylesheet( register stylesheet)
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css',array(), '8.0.0');
+    wp_register_style('googlefont','https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway:400,700,900&display=swap',array(),'1.0.0');
     wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css',array(), '4.7.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css',array('normalize'), '1.0');
 
@@ -9,6 +10,7 @@ function lapizzeria_styles(){
     wp_enqueue_style('normalize');
     wp_enqueue_style('fontawesome');
     wp_enqueue_style('style');
+    wp_enqueue_style('googlefont');
 
 
     //adding scripts//
