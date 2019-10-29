39cb550d793fd7d2ad884b489243d4a5ef4755a4
BlerinaP <blerina1.p@gmail.com>
2019-10-29 12:12:58 +0100

    Working with index.php file

--- themes/lapizzeria/index.php
+++ themes/lapizzeria/index.php
@@ -1,3 +1,26 @@
-<?php
- echo "Hello from my new wordpress theme"
-?>
+<?php get_header();?>
+
+ <?php
+    $blog_page = get_option('page_for_posts');
+    $image = get_post_thumbnail_id($blog_page);
+    $image = wp_get_attachment_image_src($image , 'full');
+  ?>
+
+
+    <div class="hero" style="background-image:url(<?php echo $image[0]; ?>);">
+        <div class="hero-content">
+            <div class="hero-text">
+                <h2><?php echo get_the_title($blog_page); ?></h2>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <main class="text-center content-text">
+           <?php while(have_posts()): the_post(); ?>
+
+           <?php endwhile; ?>
+        </main>
+    </div>
+
+<?php get_footer();?>
