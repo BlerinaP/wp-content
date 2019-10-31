4979dd3f618dfa8ae72e5567e753f3912d2fc451
BlerinaP <blerina1.p@gmail.com>
2019-10-31 10:57:53 +0100

    Adding a subMenu page

--- themes/lapizzeria/inc/options.php
+++ themes/lapizzeria/inc/options.php
@@ -2,10 +2,18 @@
 
 function lapizzeria_options(){
  add_menu_page('La Pizzeria', 'La Pizzeria Options', 'administrator', 'lapizzeria_options', 'lapizzeria_adjustments', '', 20);
-}
-add_action('admin_menu','lapizzeria_options');
 
- function lapizzeria_adjustments(){
-  echo "hello from lapizzeria_adjustmens";
+ add_submenu_page('lapizzeria_options', 'Reservations', 'Reservations', 'administrator', 'lapizzeria_reservations', 'lapizzeria_reservations');
 }
+
+  add_action('admin_menu','lapizzeria_options');
+
+
+   function lapizzeria_adjustments(){
+       echo "Hello from lapizzeria_adj!";
+   }
+
+   function lapizzeria_reservations() {
+       echo "Hello from lapizzeria_reservations!";
+   }
 ?>
