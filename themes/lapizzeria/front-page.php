64f9b9c483bc111c2a9db23d46547bb413063cb6
BlerinaP <blerina1.p@gmail.com>
2019-11-01 10:47:07 +0100

    Adding a simple map to the project.

--- themes/lapizzeria/front-page.php
+++ themes/lapizzeria/front-page.php
@@ -75,8 +75,24 @@
                     <?php get_template_part('templates/reservation'); ?>
                 </div>
         </div>
-
     </section>
+
+    <style>
+        #map{
+            height: 400px;
+        }
+    </style>
+    <script>
+        var map;
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: -34.397, lng: 150.644},
+                zoom: 8
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZijPWJzs5EZvKqFVv6-mYGTKaiJoX7oE&callback=initMap"
+            async defer></script>
 
 
 
