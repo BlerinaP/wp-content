02bd0bba348f4167933d360b092329f8b45041b1
BlerinaP <blerina1.p@gmail.com>
2019-11-02 12:41:53 +0100

    Making theme web app compatible for IOS.

--- themes/lapizzeria/header.php
+++ themes/lapizzeria/header.php
@@ -1,52 +1,57 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>La Pizzeria</title>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>La Pizzeria</title>
+        <meta name="viewport" content="width-device-width, intial-scale=1"
+         <!----Make this IOS compatible--->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="La Pizzeria Restaurant">
+        <link rel="apple-touch-icon" href="<?php echo get_template_directory() ?>/apple-touch-icon.jpg">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
 
-<header class="site-header">
-   <div class="container">
-      <div class="logo">
-             <a href="<?php echo esc_url(home_url('/') );?>">
-                <img src="<?php echo get_template_directory_uri();?>/img/logo.svg" class="logoimage">
-              </a>
-        </div> <!-- logo-->
-          <div class="header-information">
-             <div class="socials">
-               <?php
-                  $args = array(
-                  'theme_location'  => 'social-menu',
+        <header class="site-header">
+           <div class="container">
+              <div class="logo">
+                     <a href="<?php echo esc_url(home_url('/') );?>">
+                        <img src="<?php echo get_template_directory_uri();?>/img/logo.svg" class="logoimage">
+                      </a>
+                </div> <!-- logo-->
+                  <div class="header-information">
+                     <div class="socials">
+                       <?php
+                          $args = array(
+                          'theme_location'  => 'social-menu',
+                          'container'       => 'nav',
+                          'container_class' => 'socials',
+                          'container_id'    => 'socials',
+                          'link_before'     => '<span class="sr-text">',
+                          'link_after'      => '</span>'
+                          );
+                        wp_nav_menu($args);
+                     ?>
+                      </div><!--socials-->
+                      <div class="address">
+                        <p><?php echo esc_html(get_option('lapizzeria_address')); ?></p>
+                        <p>Phone Number: <?php echo esc_html(get_option('lapizzeria_phonenumber')); ?> </p>
+                      </div>
+                  </div><!--header-information-->
+           </div>
+        </header>
+        <div class="main-menu">
+            <div class="mobile-menu">
+                <a href="#"class="mobile"><i class="fa fa-bars"></i> Menu</a>
+            </div>
+            <div class="navigation container">
+                <?php
+                $args=array(
+                  'theme_location'  => 'header-menu',
                   'container'       => 'nav',
-                  'container_class' => 'socials',
-                  'container_id'    => 'socials',
-                  'link_before'     => '<span class="sr-text">',
-                  'link_after'      => '</span>'
-                  );
+                  'container_class' => 'site-nav'
+                );
                 wp_nav_menu($args);
-             ?>
-              </div><!--socials-->
-              <div class="address">
-                <p><?php echo esc_html(get_option('lapizzeria_address')); ?></p>
-                <p>Phone Number: <?php echo esc_html(get_option('lapizzeria_phonenumber')); ?> </p>
-              </div>
-          </div><!--header-information-->
-   </div>
-</header>
-<div class="main-menu">
-    <div class="mobile-menu">
-        <a href="#"class="mobile"><i class="fa fa-bars"></i> Menu</a>
-    </div>
-    <div class="navigation container">
-        <?php
-        $args=array(
-          'theme_location'  => 'header-menu',
-          'container'       => 'nav',
-          'container_class' => 'site-nav'
-        );
-        wp_nav_menu($args);
-        ?>
-    </div>
-</div>
+                ?>
+            </div>
+        </div>
